43370b80a1833aa9ed6f51f8402db7038564e471
Andrew Han <andrewthehan@gmail.com>
2022-02-25 15:50:01 -0800

    Go back to babel.

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BoundingBox } from "./src/BoundingBox";
-export { default as Location } from "./src/Location";
-export { default as Size } from "./src/Size";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as BoundingBox } from "./BoundingBox";
+export { default as Location } from "./Location";
+export { default as Size } from "./Size";
